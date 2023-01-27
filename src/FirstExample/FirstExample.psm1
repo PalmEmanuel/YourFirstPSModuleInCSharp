96eef0fd2ac7b89cb1954e31326893765fe37634
Justin Grote <JustinGrote@users.noreply.github.com>
2023-01-27 19:20:14 +0000

    More comments to first example

--- src/FirstExample/FirstExample.psm1
+++ src/FirstExample/FirstExample.psm1
@@ -7,9 +7,17 @@ function Test-IPNetworkOverlap {
 	.SYNOPSIS
 	Test if two IP networks overlap each other
 	#>
+
+	#NOTE:Parameters still need fully qualified type names even if you have using namespace at the top, however you can omit System, so System.Net.IPNetwork becomes Net.IPNetwork
 	param(
-		[Parameter(Mandatory)][IPNetwork]$Network,
-		[Parameter(Mandatory, ValueFromPipeline)][IPNetwork]$InputObject
+		#The network or networks to compare against -CompareNetwork
+		[Parameter(Mandatory, ValueFromPipeline)][Net.IPNetwork]
+		$Network,
+
+		#The network to test if the networks overlap
+		[Parameter(Position = 0, Mandatory)][Net.IPNetwork]
+		$CompareNetwork
+
 	)
 
 	process {
