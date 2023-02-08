293f42c9bc47159794abba4b25cb9259c6f31c2d
Emanuel Palm <emanuel+github@pipe.how>
2023-02-08 13:50:20 +0000

    Lorem.NET with random colors example

--- src/FirstExample/FirstExample.psm1
+++ src/FirstExample/FirstExample.psm1
@@ -2,6 +2,17 @@ using namespace System.Net
 #We will fix this to be redistributable later, for now this works with the default dotnet publish
 Add-Type -Path (Join-Path $PSScriptRoot 'bin/Debug/net7.0/publish/*.dll')
 
+function Get-RandomColor {
+	return "#$([LoremNET.Lorem]::HexNumber(6))"
+}
+
+function Set-RandomPSColors {
+	[CmdletBinding()]
+	$UnchangeableColors = @('DefaultTokenColor')
+	(Get-PSReadLineOption).psobject.properties.name.Where{$_ -like '*Color' -and $_ -notin $UnchangeableColors} |
+		ForEach-Object { $Colors = @{} } { $Colors[$($_ -replace 'Color')] = Get-RandomColor } -End { Set-PSReadLineOption -Colors $Colors }
+}
+
 function Test-IPNetworkOverlap {
 	<#
 	.SYNOPSIS
@@ -25,4 +36,4 @@ function Test-IPNetworkOverlap {
 	}
 }
 
-Export-ModuleMember Test-IPNetworkOverlap
+Export-ModuleMember Test-IPNetworkOverlap,Set-RandomPSColors
