6c4788aa986d3bfb90bc898ebe726794667451ee
Emanuel Palm <emanuel+github@pipe.how>
2023-04-11 21:52:25 +0200

    do not remove directory recursively with force, in case people specify C:\ or something accidentally

--- src/Example.6.PowerShell.MultiTarget/build.ps1
+++ src/Example.6.PowerShell.MultiTarget/build.ps1
@@ -7,12 +7,8 @@ param(
 	$OutDir = $(Join-Path $PSScriptRoot '/out/lib')
 )
 
-# Remove output directory with files if it exists
-if (Test-Path $OutDir) {
-    Remove-Item $OutDir -Recurse -Force -ErrorAction Stop
-}
 # (Re-)create output directory for copying files
 New-Item -Path $OutDir -ItemType Directory -ErrorAction Ignore
 
 # Copy files from build path to output directory
-Get-ChildItem -Path $Source | Copy-Item -Destination $OutDir
+Get-ChildItem -Path $Source | Copy-Item -Destination $OutDir -Force
