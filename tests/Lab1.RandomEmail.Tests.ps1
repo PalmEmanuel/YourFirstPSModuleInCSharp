b14ceace0614e31117ba06459cbb1365ebcfb967
Justin Grote <JustinGrote@users.noreply.github.com>
2023-06-07 17:18:45 +0000

    Split into separate describe

--- tests/Lab1.RandomEmail.Tests.ps1
+++ tests/Lab1.RandomEmail.Tests.ps1
@@ -26,21 +26,26 @@ Describe 'Lab 1: Return a random email' {
       $actual | Should -Not -Contain '.'
     }
   }
+}
 
-  Context 'Get-PEURandomEmail' {
+Describe 'Lab 2: Get a random sentence' {
+  Context 'Get-PEURandomSentence' {
     It -Skip 'Generates a Random Sentence between 5 and 10 words' {}
     It -Skip 'Generates a sentence between -Min and -Max'
     It -Skip '-Name ends the sentence with ", {Name}"'
     It -Skip 'Names provided via pipeline produce 3 separate sentences ending in the persons name'
   }
+}
 
+Describe 'Lab 3: Bouncer Script' {
   Context 'Get-PEUAge' {
     It -Skip 'Generates a random birthday for the subject'
     It -Skip 'Generates random birthdays for each name provided via the pipeline'
   }
+
   Context 'Test-PEUAge' {
     It -Skip 'Derives from Get-PEUAge'
     It -Skip 'Returns null if the user is functional'
-    It -Skip 'Throws InvalidOperationException if the specified user is under 18'
+    It -Skip 'Throws InvalidOperationException if the specified user is under -Age'
   }
 }
