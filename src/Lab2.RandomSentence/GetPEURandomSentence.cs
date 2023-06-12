1d1e6c8a246a70adab4664c113d40db79426adda
Emanuel Palm <emanuel+github@pipe.how>
2023-06-12 10:50:12 +0200

    lab 1 and 2 examples

---
+++ src/Lab2.RandomSentence/GetPEURandomSentence.cs
@@ -0,0 +1,30 @@
+﻿using System.Management.Automation;
+using LoremNET;
+
+namespace Lab2.RandomSentence;
+
+[Cmdlet(VerbsCommon.Get, "PEURandomSentence")]
+public class RandomSentenceCommand : PSCmdlet
+{
+    [Parameter(Position = 0)]
+    public int Minimum { get; set; } = 5;
+
+    [Parameter(Position = 1)]
+    public int Maximum { get; set; } = 10;
+
+    [Parameter(Position = 2, ValueFromPipeline = true)]
+    public string Name { get; set; }
+
+    protected override void EndProcessing()
+    {
+        if (!string.IsNullOrWhiteSpace(Name)) {
+            for (int i = 0; i < 3; i++) {
+                WriteObject(Lorem.Sentence(Minimum, Maximum).Replace(".", $", {Name}"));
+            }
+        }
+        else
+        {
+            WriteObject(Lorem.Sentence(Minimum, Maximum));
+        }
+    }
+}
