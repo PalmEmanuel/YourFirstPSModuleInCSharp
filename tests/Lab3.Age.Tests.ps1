202672ab0c7a6f4f7bcb07e572b4e33ea3c9704d
Justin Grote <JustinGrote@users.noreply.github.com>
2023-06-13 14:33:07 +0200

    Add initial Lab3 Test scaffolding

--- tests/Lab3.Age.Tests.ps1
+++ tests/Lab3.Age.Tests.ps1
@@ -1,7 +1,41 @@
 Describe -Skip 'Lab 3: Bouncer Script' {
+  BeforeAll {
+    $SCRIPT:ProjectName = 'Lab.3.Age'
+    $ErrorActionPreference = 'Stop'
+    $SCRIPT:ProjectPath = Join-Path $PSScriptRoot "../src/$ProjectName"
+    $SCRIPT:PublishPath = Join-Path $ProjectPath 'bin/Debug/net6.0/publish'
+    if (-not (Test-Path "$ProjectPath/$ProjectName.csproj")) {
+      throw [NotImplementedException]"This lab has not been initialized yet. Hint: dotnet new classlib -f net6.0 -o $ProjectPath"
+    }
+    $SCRIPT:PackageList = dotnet list $ProjectPath package
+    if (-not $PackageList -match 'System.Management.Automation.*7.2') {
+      throw [NotImplementedException]"This lab has does not have the System.Management.Automation v7.2 package added for PowerShell module development. Hint: dotnet add $ProjectPath package System.Management.Automation --version 7.2.11"
+    }
+    if (-not $PackageList -match 'Lorem.Universal.Net') {
+      throw [NotImplementedException]"This lab does not have the Lorem Universal package added. Hint: dotnet add $ProjectPath package Lorem.Universal.Net"
+    }
+    & dotnet publish $ProjectPath
+    Import-Module (Join-Path $PublishPath "$ProjectName.dll")
+  }
+
   Context 'Get-PEUAge' {
-    It 'Generates a random birthday for the subject' {}
-    It 'Generates random birthdays for each name provided via the pipeline' {}
+    It 'Generates a random birthday for the subject' {
+      $actual = Get-PEUAge -Name 'PSConfEUParticipant'
+      $actual.name | Should -Be 'PSConfEUParticipant'
+      $actual.birthday | Should -BeOfType [DateTime]
+    }
+    It 'Generates random birthdays for each name provided via the pipeline' {
+      $names = 'PSConfEUParticipant', 'PSConfEUParticipant2', 'PSConfEUParticipant3'
+      $actual = $names | Get-PEUAge
+      $actual.Count | Should -BeExactly $names.Count
+      $names | ForEach-Object {
+        $actual.name | Should -Contain $_
+      }
+      $actual.birthday.count | Should -BeExactly $names.Count
+      $actual.birthday | ForEach-Object {
+        $PSItem | Should -BeOfType [DateTime]
+      }
+    }
   }
 
   Context 'Test-PEUAge' {
