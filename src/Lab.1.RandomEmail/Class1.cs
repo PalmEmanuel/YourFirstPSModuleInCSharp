fd994b674b7c8b71b9f22449a309ec8531892b98
T-JGrote <135832474+T-JGrote@users.noreply.github.com>
2023-06-14 06:52:37 +0000

    First Lab Done

---
+++ src/Lab.1.RandomEmail/Class1.cs
@@ -0,0 +1,18 @@
+﻿namespace PEURandom;
+using System.Management.Automation;
+using System.Net.Mail;
+using LoremNET;
+
+[Cmdlet(VerbsCommon.Get, $"PEURandomEmail")]
+public class GetPEURandomEmailCmdlet : PSCmdlet
+{
+    [Parameter()]
+    public SwitchParameter UsernameOnly;
+
+    protected override void EndProcessing()
+    {
+        MailAddress email = new(Lorem.Email());
+        string output = UsernameOnly ? email.User : email.ToString();
+        WriteObject(output);
+    }
+}
